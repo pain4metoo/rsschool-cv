3efea76b80c5fdcc51312c8bd79712f5576b51ea
pain4metoo <pain4metoo@gmail.com>
2022-07-22 01:18:05 +0200

    feat: add scroll animation on skills and fix header transition

--- src/ts/main/anim_skills.ts
+++ src/ts/main/anim_skills.ts
@@ -1,10 +1,32 @@
-const skillsGrafic = document.querySelectorAll(".main_skills_circle");
-const clientSkroll = window.scroll();
+const skillsGrafic = document.getElementsByClassName(
+  "anim"
+) as HTMLCollectionOf<HTMLElement>;
 
-(function getCor() {
+function getCor() {
   for (let i = 0; i < skillsGrafic.length; i++) {
-    let cor = skillsGrafic[i].getBoundingClientRect();
+    let el = skillsGrafic[i];
+    const height = el.offsetHeight;
+    const offSet = getOffSet(el).top;
+    const animNum = 4;
 
-    console.log(cor);
+    console.log(offSet);
+    let point = window.innerHeight - height / animNum;
+
+    if (window.pageYOffset > offSet - point) {
+      el.classList.add("active");
+    }
   }
-})();
+}
+
+function getOffSet(el) {
+  const rect = el.getBoundingClientRect();
+  const scrollLeft = window.pageXOffset;
+  const scrollTop = window.pageYOffset;
+
+  return {
+    top: rect.top + scrollTop,
+    letf: rect.left + scrollLeft,
+  };
+}
+
+window.addEventListener("scroll", getCor);
